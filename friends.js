43ad85bdefefe911acc398876fa9740e9b504876
Devynne Marshall <devynnem@gmail.com>
2022-12-07 20:28:48 -0700

    Add new additions

--- friends.js
+++ friends.js
@@ -2,3 +2,7 @@ var arrays = ["Bob", "Sue", "Bill", "Joe", "Adam"]
     console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var vacations = ["Japan", "Nepal", "Mexico", "Turkey", "Canada"]
+vacations.pop();
+console.log(vacations);
